ba0fc97d7ad47a50e4b0f36670fbe5c9f7677163
luckyamina <144383353+luckyamina@users.noreply.github.com>
2023-11-03 19:27:03 +0400

    Add files via upload

---
+++ leasson3.py
@@ -0,0 +1,10 @@
+# Here input asks for user's name and surname
+
+User_name = input("Enter your name: ")
+User_surname = input("Enter your surname: ")
+print("Hello " + User_name + " " + User_surname)
+
+# Input asks for user's age and prints out user's age in 25 years later. 
+User_age = input("Enter your age: ")
+print(User_age)
+print("25 years later, You'll be",int(User_age) + 25,"years old")
